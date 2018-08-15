e08de33a3cef76039a1c961833bd6217bbe818e9
GabrielDuke <duke.comandos@gmail.com>
2018-08-14 23:29:59 -0300

    teste da madrugada

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<DOCTYPE html>
+<!DOCTYPE html>
   <html>
   <head>
-
+    
     <link rel="stylesheet" type="text/css" href="./css/style.css"> 
   </head>
 
-
-
-   
   <body>
-
+ 
  <h1>Game Overload</h1>
   <p>Garanta o seu ingresso e curta todas as 
       novidades e lançamentos do 
@@ -17,17 +14,29 @@
        novas tecnologias, campeonatos e 
        muita diversão!
         Vem jogar com a gente!</p>
-        <nav class="bg-preto">
+        <nav>
             <ul class="">
               <li><a href="index.php">Inicio</a></li>
-              <li><a href="?produtos">Evento</a></li>
-              <li><a href="?listarCliente">Participantes</a></li>
+              <li><a href="">Evento</a></li>
+              <li><a href="">Participantes</a></li>
             </ul>
             
-            <ul class="direita">
-              <li><a href="?formCliente">inscriçao</a></li>
+            <ul class="">
+              <li><a href="?formCadastro">inscriçao</a></li>
               <li><a href="?formLogin">Login</a></li>
             </ul>
           </nav>
+
+<section class="container">
+		<?php
+			if (!empty($_GET)){
+				$p = key($_GET);
+				//include "page/".$p.".php";
+				include "page/$p.php";
+			}	
+		?>
+	</section>
+
+
 </body>
   </html>
---
+++ page/formLogin.php
@@ -0,0 +1,12 @@
+<form>
+<label for="">E-mail</label>
+	<input type="email" name="email">
+	
+	<label for="">Senha</label>
+	<input type="password" name="pws1">
+    
+    <label>
+		<button> Logar </button>
+		<button type="reset"> Cancelar </button>
+	</label>
+</form>
